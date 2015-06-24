c444e4fffac9f3ea6021cdac59a3b97e1a6f79fe
Max Norton <maxbnorton@gmail.com>
2015-06-24 12:49:20 -0700

    kill autoscroll when hero content button is clicked

--- assets/js/min/scripts.js
+++ assets/js/min/scripts.js
@@ -11,9 +11,12 @@ jQuery(document).ready(function() {
 
    jQuery('body').append('<div id="skrollr-body"></div>');
 
+   var killAutoscroll = false;
+
 	jQuery('.scroll-to-anchor').each( function() {
 		jQuery(this).click( function() {
 			var target = jQuery(this).attr('href');
+			killAutoscroll = true;
 			jQuery('html, body').animate({
 		        scrollTop: jQuery(target).offset().top
 		    }, 750);
@@ -25,7 +28,7 @@ jQuery(document).ready(function() {
 		jQuery(window).scroll(function() {
 			var scrollTop = jQuery(window).scrollTop();
 			var contentTop = Math.round( jQuery('#content').offset().top - 30 );
-			if ( scrollTop > lastScroll &&  scrollTop < contentTop - 100) {
+			if ( scrollTop > lastScroll &&  scrollTop < contentTop - 100 && !killAutoscroll) {
 				jQuery('html, body').animate({
 			        scrollTop: contentTop
 				    }, 250, function () {
